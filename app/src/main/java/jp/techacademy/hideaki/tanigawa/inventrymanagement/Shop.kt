48ed308d505b0433ec3c65e2150fb9f33e7f8b8e
hideaki-tanigawa <h-tanigawa@i-enter.co.jp>
2023-06-15 10:52:19 +0900

    fix #7

--- app/src/main/java/jp/techacademy/hideaki/tanigawa/inventrymanagement/Shop.kt
+++ app/src/main/java/jp/techacademy/hideaki/tanigawa/inventrymanagement/Shop.kt
@@ -1,5 +1,6 @@
 package jp.techacademy.hideaki.tanigawa.inventrymanagement
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Base64
@@ -21,6 +22,7 @@ class Shop:Fragment() {
     private lateinit var adapter: InventryListAdapter
     private lateinit var shopRef: DatabaseReference
     private lateinit var userRef: DatabaseReference
+    private lateinit var invRef:DatabaseReference
 
     private val groupList = arrayListOf<String>()
 
@@ -86,7 +88,6 @@ class Shop:Fragment() {
             var inventryId = ""
             var price = ""
             var count = ""
-            Log.d("Test",snapshot.value.toString())
             for(list in groupList){
                 Log.d("Group-Test",list)
                 if(groupId.equals(list)){
@@ -120,14 +121,49 @@ class Shop:Fragment() {
                     )
                     shopListArrayList.add(inventry)
                     adapter.notifyDataSetChanged()
+
+                    // ListViewを長押しした時の処理
+                    binding.listView.setOnItemLongClickListener{parent, _, position, _ ->
+                        // 在庫品を削除する
+                        val inventry = parent.adapter.getItem(position) as Inventry
+
+                        // ダイアログを表示する
+                        val builder = AlertDialog.Builder(context)
+
+                        builder.setTitle("在庫登録 or 削除")
+                        builder.setMessage(commodity + "を登録 or 削除しますか")
+                        builder.setPositiveButton("削除") {_, _ ->
+                            deleteInventryListInfo(groupId)
+                        }
+
+                        builder.setNegativeButton("登録", null)
+
+                        builder.setNeutralButton("キャンセル",null)
+
+                        val dialog = builder.create()
+                        dialog.show()
+                        true
+                    }
                 }
                 override fun onCancelled(error: DatabaseError) {}
 
             })
+
+            // リストが長押しされた際に発火
+
         }
         override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
         override fun onChildRemoved(snapshot: DataSnapshot) {}
         override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
         override fun onCancelled(error: DatabaseError) {}
+    }
+
+    /**
+     * Firebaseから在庫データを削除する処理
+     */
+    private fun deleteInventryListInfo(groupID: String){
+        invRef = databaseReference.child(ShoppingPATH).child(groupID)
+        invRef!!.removeValue()
+        onResume()
     }
 }
